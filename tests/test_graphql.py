f3de9b2c88c200c59ebf64a650c965e7e7a399ef
Rob Rudin <rudin@progress.com>
2023-10-05 15:55:13 -0400

    Merged `graphql` function into `query` function.
    This seems more expected to a user, who can now use a single function and select the type of query they want to submit.

--- tests/test_graphql.py
+++ tests/test_graphql.py
@@ -1,6 +1,6 @@
 def test_graphql(client):
-    data = client.rows.graphql(
-        "query musicianQuery { test_musician { lastName firstName dob } }"
+    data = client.rows.query(
+        graphql="query musicianQuery { test_musician { lastName firstName dob } }"
     )
     musicians = data["data"]["test_musician"]
     assert 4 == len(musicians)
@@ -8,8 +8,8 @@ def test_graphql(client):
 
 
 def test_graphql_return_response(client):
-    response = client.rows.graphql(
-        "query musicianQuery { test_musician { lastName firstName dob } }",
+    response = client.rows.query(
+        graphql="query musicianQuery { test_musician { lastName firstName dob } }",
         return_response=True,
     )
     assert 200 == response.status_code
@@ -20,8 +20,8 @@ def test_graphql_return_response(client):
 
 
 def test_graphql_bad_graphql(client):
-    response = client.rows.graphql(
-        "query musicianQuery { test_musician { lastName firstName dob } "
+    response = client.rows.query(
+        graphql="query musicianQuery { test_musician { lastName firstName dob } "
     )
     assert 1 == len(response["errors"])
     assert (
@@ -31,7 +31,7 @@ def test_graphql_bad_graphql(client):
 
 
 def test_graphql_bad_user(not_rest_user_client):
-    response = not_rest_user_client.rows.graphql(
-        "query musicianQuery { test_musician { lastName firstName dob } }"
+    response = not_rest_user_client.rows.query(
+        graphql="query musicianQuery { test_musician { lastName firstName dob } }"
     )
     assert 403 == response.status_code
