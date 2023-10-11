d480684b57e0d5cb814e275c9dc9d1c86dbfdd2e
Rob Rudin <rudin@progress.com>
2023-10-11 09:05:08 -0400

    Added impl and test for non-document binary()
    Also moved the primitive_value_converters below the main public function.

--- marklogic/internal/eval.py
+++ marklogic/internal/eval.py
@@ -9,23 +9,6 @@ from requests_toolbelt.multipart.decoder import MultipartDecoder
 Supports working with data returned by the v1/eval and v1/invoke endpoints.
 """
 
-__primitive_value_converters = {
-    "integer": lambda part: int(part.text),
-    "decimal": lambda part: Decimal(part.text),
-    "boolean": lambda part: ("False" == part.text),
-    "string": lambda part: part.text,
-    "map": lambda part: json.loads(part.text),
-    "element()": lambda part: part.text,
-    "array": lambda part: json.loads(part.text),
-    "array-node()": lambda part: json.loads(part.text),
-    "object-node()": lambda part: __process_object_node_part(part),
-    "document-node()": lambda part: __process_document_node_part(part),
-    # It appears that binary() will only be returned for a binary node retrieved
-    # from the database, and thus an X-URI will always exist. Have not found a
-    # scenario that indicates otherwise.
-    "binary()": lambda part: Document(__get_decoded_uri_from_part(part), part.content),
-}
-
 
 def process_multipart_mixed_response(response: Response) -> list:
     """
@@ -55,20 +38,28 @@ def process_multipart_mixed_response(response: Response) -> list:
     return transformed_parts
 
 
-def __get_decoded_uri_from_part(part):
-    encoding = part.encoding
-    return part.headers["X-URI".encode(encoding)].decode(encoding)
+__primitive_value_converters = {
+    "integer": lambda part: int(part.text),
+    "decimal": lambda part: Decimal(part.text),
+    "boolean": lambda part: "False" == part.text,
+    "string": lambda part: part.text,
+    "map": lambda part: json.loads(part.text),
+    "element()": lambda part: part.text,
+    "array": lambda part: json.loads(part.text),
+    "array-node()": lambda part: json.loads(part.text),
+    "object-node()": lambda part: __process_node(
+        part, lambda part: json.loads(part.text)
+    ),
+    "document-node()": lambda part: __process_node(part, lambda part: part.text),
+    "binary()": lambda part: __process_node(part, lambda part: part.content),
+}
 
 
-def __process_object_node_part(part):
+def __process_node(part, content_extractor):
+    content = content_extractor(part)
     if b"X-URI" in part.headers:
-        return Document(__get_decoded_uri_from_part(part), json.loads(part.text))
+        encoding = part.encoding
+        uri = part.headers["X-URI".encode(encoding)].decode(encoding)
+        return Document(uri, content)
     else:
-        return json.loads(part.text)
-
-
-def __process_document_node_part(part):
-    if b"X-URI" in part.headers:
-        return Document(__get_decoded_uri_from_part(part), part.text)
-    else:
-        return part.text
+        return content
