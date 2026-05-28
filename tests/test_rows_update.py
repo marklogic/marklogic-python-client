bfcc3a110819da78b8f807efe90dea0a6f381610
Rita Chen <ritac@progress.com>
2026-05-28 15:55:28 -0400

    MLE-29695 updated Copyright

--- tests/test_rows_update.py
+++ tests/test_rows_update.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023-2025 Progress Software Corporation and/or its subsidiaries or affiliates. All Rights Reserved.
+# Copyright (c) 2023-2026 Progress Software Corporation and/or its subsidiaries or affiliates. All Rights Reserved.
 
 
 import json
